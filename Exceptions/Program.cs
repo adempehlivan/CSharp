1085246f622aea578b732d8e9385da1b45121580
Adem Pehlivan <adem.pehlivan@ajanspress.com.tr>
2018-11-15 14:04:06 +0300

    Exceptions Action and Result

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Exceptions
@@ -12,6 +13,43 @@ namespace Exceptions
         {
             //ExeptionIntro();
 
+            ////TryCatch();
+
+            //ActionDemo();
+
+            Func<int, int, int> add = Topla; // Fucn ile method gönderme
+            Console.WriteLine(add(3,5));
+
+            Func<int> getRandomNumber = delegate() //parametresiz methoda delege , delegasyon yapar
+            {//aynı action gibi func isteyebiliriz.
+                Random random = new Random();
+                return random.Next(1, 100);
+            };
+
+            Func<int> getRandomNumber2 = () => new Random().Next(1, 100); //  action yazılımı gibi üsttekinin kısası
+
+            Console.WriteLine(getRandomNumber());
+            Thread.Sleep(1000);//random sayılar farklı gelsin diye öylesine eklendi.
+            Console.WriteLine(getRandomNumber2());
+            Console.ReadLine();
+
+        }
+
+        static int Topla(int x,int y)
+        {
+            return x + y;
+        }
+
+        private static void ActionDemo()
+        {
+            HandleException(() =>
+            {
+                Find();
+            });
+        }
+
+        private static void TryCatch()
+        {
             try
             {
                 Find();
@@ -20,13 +58,6 @@ namespace Exceptions
             {
                 Console.WriteLine(exception.Message);
             }
-
-            HandleException(() => {
-                Find();
-            });
-
-            Console.ReadLine();
-
         }
 
         private static void HandleException(Action action)
